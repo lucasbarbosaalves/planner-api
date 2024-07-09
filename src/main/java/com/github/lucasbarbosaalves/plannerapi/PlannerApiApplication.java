7baf467aa30c55d5f86304a8e4526fba1b918071
lucasbalves <lkab05@hotmail.com>
2024-07-08 21:46:21 -0300

    setup

---
+++ src/main/java/com/github/lucasbarbosaalves/plannerapi/PlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasbarbosaalves.plannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lucasbarbosaalves/plannerapi/PlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasbarbosaalves.plannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
