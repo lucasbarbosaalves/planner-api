07efc5f2e2308d8edadc6529bab2ecda308e23cc
lucasbalves <lkab05@hotmail.com>
2024-07-09 13:16:03 -0300

    refactor: adjusted controller layer

--- src/main/java/com/github/lucasbarbosaalves/plannerapi/controller/TripController.java
+++ src/main/java/com/github/lucasbarbosaalves/plannerapi/controller/TripController.java
@@ -5,9 +5,13 @@ import com.github.lucasbarbosaalves.plannerapi.entity.trip.Trip;
 import com.github.lucasbarbosaalves.plannerapi.entity.trip.dto.TripCreateResponse;
 import com.github.lucasbarbosaalves.plannerapi.entity.trip.dto.TripRequestPayload;
 import com.github.lucasbarbosaalves.plannerapi.repository.TripRepository;
+import com.github.lucasbarbosaalves.plannerapi.service.TripService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,25 +20,30 @@ import java.util.UUID;
 @RequestMapping("/trips")
 public class TripController {
 
-    private final ParticipantService participantService;
-    private final TripRepository repository;
+   private final TripService tripService;
 
-    public TripController(ParticipantService participantService, TripRepository repository) {
-        this.participantService = participantService;
-        this.repository = repository;
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
     }
 
     @PostMapping
     public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
-        Trip newTrip = new Trip(payload);
-        this.repository.save(newTrip);
-        this.participantService.registerParticipantsToTrip(payload.emails_to_invite(), newTrip.getId());
-        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+        return tripService.create(payload);
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public ResponseEntity<Trip> getTripDetails(@PathVariable(name = "id") UUID id) {
-        Optional<Trip> trip = this.repository.findById(id);
-        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return tripService.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable(name = "id") UUID id, @RequestBody TripRequestPayload payload) {
+        return tripService.update(id, payload);
+    }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable(name = "id") UUID id) {
+        return tripService.confirm(id);
     }
 }
